6cd8c774944f233c23e92df7bf6bacc38887ab87
Simen Heggestøyl <simen.heggestoyl@byr.oslo.kommune.no>
2020-02-27 11:01:02 +0100

    Really turn off automatic sliding

--- src/views/Slider.vue
+++ src/views/Slider.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="container-fluid px-0">
-    <div id="carousel-controls" class="carousel slide" data-ride="carousel">
+    <div id="carousel-controls" class="carousel slide" data-ride="carousel" data-interval="">
       <div class="carousel-inner bg-info" role="listbox">
-        <div class="carousel-item active" data-interval="">
+        <div class="carousel-item active">
           <div class="d-flex align-items-center justify-content-center min-vh-100">
             <h1 class="display-1">ONE</h1>
           </div>
         </div>
-        <div class="carousel-item" data-interval="">
+        <div class="carousel-item">
           <div class="d-flex align-items-center justify-content-center min-vh-100">
             <h1 class="display-1">TWO</h1>
           </div>
         </div>
-        <div class="carousel-item" data-interval="">
+        <div class="carousel-item">
           <div class="d-flex align-items-center justify-content-center min-vh-100">
             <h1 class="display-1">THREE</h1>
           </div>
