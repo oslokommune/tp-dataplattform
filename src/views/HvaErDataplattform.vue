dee6b4046d25f401f24a140e23d32d160db48c1f
Kristoffer Strekerud <kristr@knowit.no>
2020-02-27 13:48:40 +0100

    Mandat

--- src/views/HvaErDataplattform.vue
+++ src/views/HvaErDataplattform.vue
@@ -1,55 +1,61 @@
 <template>
-    <div class="content">
-        <div>
-            <h1>Hva er dataplattformen?</h1>
+  <div class="content">
+    <div>
+      <h1>Hva er dataplattformen?</h1>
 
-            <p>
-                Med dataplattformen tilbyr vi en rekke verktøy og tjenester som gjør det lett å skape verdi med data på
-                en måte som ivaretar personvern og individets rettigheter. Vi lever etter prinsippene om at:
-            </p>
+      <p>Dataplattformen er en samling av tilgjengelige data i Oslo Kommune.</p>
 
-            <ul>
-                <li><Icons arrow />Livet er en strøm av hendelser</li>
-                <li><Icons arrow />Innsikt om innbyggerne skaper bedre tjenester</li>
-            </ul>
+      <p>
+        Med dataplattformen tilbyr vi en rekke verktøy og tjenester som gjør det lett å skape verdi med data på
+        en måte som ivaretar personvern og individets rettigheter. Vi lever etter prinsippene om at:
+      </p>
 
-            <p>
-                Med dataplattformen får du for eksempel få tilgang til et datasett med informasjon om vannkvaliteten på
-                Oslo kommune sine badeplasser.
-            </p>
+      <ul>
+        <li>
+          <Icons arrow />Livet er en strøm av hendelser
+        </li>
+        <li>
+          <Icons arrow />Innsikt om innbyggerne skaper bedre tjenester
+        </li>
+      </ul>
 
-            <img src="~@/assets/dataeksempel.png" />
-        </div>
+      <p>
+        Med dataplattformen får du for eksempel få tilgang til et datasett med informasjon om vannkvaliteten på
+        Oslo kommune sine badeplasser.
+      </p>
+
+      <img src="~@/assets/dataeksempel.png" />
     </div>
+  </div>
 </template>
 
 <script>
-  import { Icons } from '@tuuturu/vue/assets'
-  export default {
-      name: "HvaErDataplattform",
-      components: {
-          Icons
-      }
+import { Icons } from "@tuuturu/vue/assets";
+export default {
+  name: "HvaErDataplattform",
+  components: {
+    Icons
   }
+};
 </script>
 
 <style scoped>
-    .content {
-        text-align: left;
-        display: flex;
-        align-items: center;
-        flex-direction: column;
-    }
+.content {
+  text-align: left;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
 
-    .Icons {
-        display: inline-block;
-    }
+.Icons {
+  display: inline-block;
+}
 
-    .list {
-        list-style: disc;
-    }
+.list {
+  list-style: disc;
+}
 
-    .list li {
-        list-style-position: inside;
-    }
+.list li {
+  list-style-position: inside;
+}
 </style>
