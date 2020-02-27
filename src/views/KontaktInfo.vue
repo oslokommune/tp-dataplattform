8e2f13cbf53ac29c839055bab5d5f71826d26a5b
Kristoffer Strekerud <kristr@knowit.no>
2020-02-27 14:08:10 +0100

    MISSING Files

---
+++ src/views/KontaktInfo.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="content">
+    <h3>LAGET AV GJENGEN®</h3>-
+    <img src="https://globalsemestersblog.files.wordpress.com/2014/02/friends.jpg" />
+  </div>
+</template>
+
+<script>
+import { Icons } from "@tuuturu/vue/assets";
+export default {
+  name: "Datasett",
+  components: {
+    Icons
+  }
+};
+</script>
+
+<style scoped>
+.content {
+  text-align: left;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  width: 50px;
+  white-space: normal;
+  word-break: break-word;
+  max-width: 1200px;
+}
+.Icons {
+  display: inline-block;
+}
+li {
+  margin: 1rem 0;
+}
+.content,
+p {
+  font-size: 24px;
+  line-height: 34px;
+}
+ul {
+  margin: 1.5rem 4rem;
+}
+h1 {
+  margin-bottom: 1.5rem;
+}
+</style>
