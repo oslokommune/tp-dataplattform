9dc97b1834260c7e8b6249ba2651ecf73203b427
Kristoffer Strekerud <kristr@knowit.no>
2020-02-27 10:17:03 +0100

    BG YOLO

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-		<h1>Værsego'! Lag magi</h1>
+    <h1>WE GOT YO DATA</h1>
   </div>
 </template>
 
@@ -8,15 +8,25 @@
 // @ is an alias to /src
 
 export default {
-  name: 'Home'
-}
+  name: "Home"
+};
 </script>
 
 <style lang="scss" scoped>
 p {
-	font-family: 'Oslo Sans', Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Oslo Sans", Avenir, Helvetica, Arial, sans-serif;
 }
 p.old {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+
+.home {
+  background-image: url("~@/assets/bg.jpg");
+  background-size: cover;
+}
+
+h1 {
+  color: white;
+  height: 55px;
 }
 </style>
