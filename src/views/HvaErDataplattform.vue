463a2c27ff6a2714be359db2559f4cd6e1e428fb
Yngvar Kristiansen <yngkri@knowit.no>
2020-02-27 11:43:51 +0100

    HvaErDataplattform rediger tekst

--- src/views/HvaErDataplattform.vue
+++ src/views/HvaErDataplattform.vue
@@ -1,7 +1,19 @@
 <template>
-    <div>
-        <h3>Dataplattformen skal sørge for trygg deling og gjenbruk av kommunens data for å skape verdi for Tim.</h3>
+    <div class="content">
+        <div>
+            <h1>Dataplattformen skal sørge for trygg deling og gjenbruk av kommunens data for å skape verdi for Tim.</h1>
 
+            <br/>
+            <h3>
+                <li>Oslo Origo skal sørge for at digitaliseringsarbeidet som pågår i Oslo kommune drar i én retning og bygger
+                    på samme plattform</li>
+            </h3>
+
+            <h3>
+                <li>Oslo Origo skal være en katalysator for innovasjon på tvers av virksomheter og sektorer, og bygge verktøy
+                og tjenester som bidrar til å realisere historien om Tim</li>
+            </h3>
+        </div>
     </div>
 </template>
 
@@ -12,5 +24,14 @@
 </script>
 
 <style scoped>
-
+    .content {
+        text-align: left;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        width:50px;
+        white-space: normal;
+        word-break: break-word;
+        max-width:1200px;
+    }
 </style>
